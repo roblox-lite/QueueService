bf244c4dc790b85149ddbb561b2f1b9701887a98
frames <68570351+workframes@users.noreply.github.com>
2021-12-11 15:47:10 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,6 +7,8 @@ local Queue = {}
 Queue.RunningQueues = {}
 Queue.__index = Queue.RunningQueues
 
+local Cache = {}
+
 --> Functions
 function Queue.New(Name : string, RefreshRate : number, MassRun : boolean)
     assert(type(Name) == "string", "Argument #1 of Queue.New must be a string.")
@@ -18,9 +20,25 @@ function Queue.New(Name : string, RefreshRate : number, MassRun : boolean)
         Queue = {},
         RefreshRate = RefreshRate,
         MassRun = MassRun,
+        Paused = false,
     }
 
-    return setmetatable(self, Queue)
+    local Metatable = setmetatable(self, Queue)
+    table.insert(Cache, Metatable)
+
+    return Metatable
+end
+
+function Queue.Fetch(Name : string)
+    assert(type(Name) == "string", "Argument #1 of Queue.Fetch must be a string.")
+
+    for Key, Value in ipairs(Cache)do
+        if(Value.Name == Name)then
+            return Value
+        end
+    end
+
+    return {}
 end
 
 function Queue.RunningQueues:Add(Function : any)
@@ -32,21 +50,45 @@ end
 function Queue.RunningQueues:Run()
     coroutine.wrap(function()
         while(task.wait(self.RefreshRate))do
-            print(self.Queue)
-            if(#self.Queue > 0)then
-                if(self.MassRun)then
-                    for Key, Value in pairs(self.Queue)do
+            if not(self.Paused)then
+                if(#self.Queue > 0)then
+                    if(self.MassRun)then
+                        for Key, Value in pairs(self.Queue)do
+                            Value()
+                            self.Queue[Key] = nil
+                        end
+                    elseif not(self.MassRun)then
+                        local Key, Value = next(self.Queue)
                         Value()
                         self.Queue[Key] = nil
                     end
-                elseif not(self.MassRun)then
-                    local Key, Value = next(self.Queue)
-                    Value()
-                    self.Queue[Key] = nil
                 end
             end
         end
     end)()
+end
+
+function Queue.RunningQueues:Clear(RunFunctions : boolean)
+    assert(type(RunFunctions) == "boolean", "Argument #1 of Queue.New must be a boolean.")
+
+    if(RunFunctions)then
+        for Key, Value in pairs(self.Queue)do
+            Value()
+            self.Queue[Key] = nil
+        end
+
+        self.Queue = {}
+    else
+        self.Queue = {}
+    end
+end
+
+function Queue.RunningQueues:Pause()
+    self.Paused = true
+end
+
+function Queue.RunningQueues:Resume()
+    self.Paused = false
 end
 
 return Queue
